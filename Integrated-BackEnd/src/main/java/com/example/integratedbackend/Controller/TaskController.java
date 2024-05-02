84accf361143ceab7e32cd09f14a35643cd8d4af
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2024-05-02 13:48:16 +0700

    updated

--- Integrated-BackEnd/src/main/java/com/example/integratedbackend/Controller/TaskController.java
+++ Integrated-BackEnd/src/main/java/com/example/integratedbackend/Controller/TaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/tasks")
-@CrossOrigin(origins = "http://localhost:80")
+@CrossOrigin(origins = {"http://localhost:80", "http://ip23nw2.sit.kmutt.ac.th:80"})
 public class TaskController {
     @Autowired
     TaskService service;
