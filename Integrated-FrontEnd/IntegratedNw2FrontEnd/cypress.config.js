8e688878b0a90d58809d39eeab61dcfea43c4516
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2024-05-02 16:01:20 +0700

    updated properties

--- Integrated-FrontEnd/IntegratedNw2FrontEnd/cypress.config.js
+++ Integrated-FrontEnd/IntegratedNw2FrontEnd/cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:5173'
+    baseUrl: 'http://localhost'
   },
   component: {
     specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
