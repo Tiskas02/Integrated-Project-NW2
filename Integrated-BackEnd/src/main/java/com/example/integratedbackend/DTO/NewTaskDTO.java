dcd2b84253a0ca94839b98c249f887340a82a44e
Tanawin <tanawin.keer@mail.kmutt.ac.th>
2024-05-07 19:13:48 +0700

    fix bug add

--- Integrated-BackEnd/src/main/java/com/example/integratedbackend/DTO/NewTaskDTO.java
+++ Integrated-BackEnd/src/main/java/com/example/integratedbackend/DTO/NewTaskDTO.java
@@ -14,19 +14,19 @@ public class NewTaskDTO {
             this.Description = description.trim();
         }
     }
-    public void setAssignees(String assignees) {
-        if(assignees != null) {
-            this.Assignees = assignees.trim();
-        }
-    }
-    public void setTitle(String title) {
-        if(title != null) {
-            this.Title = title.trim();
-        }
-    }
-    public void setStatus(ResourceType status) {
-        if(status == null ) {
-            this.Status = ResourceType.NO_STATUS;
-        }
-    }
+//    public void setAssignees(String assignees) {
+//        if(assignees != null) {
+//            this.Assignees = assignees.trim();
+//        }
+//    }
+//    public void setTitle(String title) {
+//        if(title != null) {
+//            this.Title = title.trim();
+//        }
+//    }
+//    public void setStatus(ResourceType status) {
+//        if(status == null ) {
+//            this.Status = ResourceType.NO_STATUS;
+//        }
+//    }
 }
