3a1f19ba024c50e521afb42f634994fa1b8d79f7
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2024-05-11 22:08:33 +0700

    Updated body

--- Integrated-FrontEnd/IntegratedNw2FrontEnd/src/components/Toaster.vue
+++ Integrated-FrontEnd/IntegratedNw2FrontEnd/src/components/Toaster.vue
@@ -1,5 +1,5 @@
 <template>
-  <Teleport to="body">
+  <Teleport to="#body">
     <Transition name="toast">
       <div v-if="toastStore.toasts.length" class="toaster__wrapper">
         <TransitionGroup name="toast" tag="ul">
