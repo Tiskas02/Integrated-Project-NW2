dc92cc44715528232c1da7ef2231106f2d1cf292
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2024-05-12 15:55:40 +0700

    Updated CrossOrigins and location

--- Integrated-BackEnd/src/main/java/com/example/integratedbackend/Controller/TaskController.java
+++ Integrated-BackEnd/src/main/java/com/example/integratedbackend/Controller/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
-@CrossOrigin(origins = {"http://localhost", "http://ip23nw2.sit.kmutt.ac.th", "http://intproj23.sit.kmutt.ac.th" ,"*"})
+@CrossOrigin(origins = "*" , allowedHeaders = "*")
 public class TaskController {
     @Autowired
     TaskService service;
