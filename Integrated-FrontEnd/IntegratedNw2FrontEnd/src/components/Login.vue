a141d015ebd0bbe92ca60ec795639a80adfdc31a
Saksit tatitrisakul <saksit.tati@mail.kmutt.ac.th>
2024-08-17 09:05:52 +0700

    update validation login form and fetch user

--- Integrated-FrontEnd/IntegratedNw2FrontEnd/src/components/Login.vue
+++ Integrated-FrontEnd/IntegratedNw2FrontEnd/src/components/Login.vue
@@ -1,35 +1,79 @@
 <script setup>
+import router from "@/router/router";
 import { ref, computed } from "vue";
-
+const url = import.meta.env.VITE_BASE_URL;
 const user = ref({
   username: "",
   password: "",
 });
+const showError = ref(false);
 
-const watchUser = computed(() => {
-  return user.value.password, user.value.username;
+const validateUsername = computed(() => {
+  return user.value.username.length <= 50;
 });
 
-const validateUsername = () => {
-  return user.value.username.length > 0 && user.value.username.length <= 50;
-}
-
-const validatePassword = () => {
-  return user.value.password.length > 0 && user.value.password.length <= 14;
-}
+const validatePassword = computed(() => {
+  return user.value.password.length <= 14;
+});
 
 // Computed property to determine if the form is valid
 const isFormValid = computed(() => {
-  return validateUsername() && validatePassword();
+  return (
+    validateUsername.value &&
+    validatePassword.value &&
+    user.value.username.length > 0 &&
+    user.value.password.length > 0
+  );
 });
+function parseJwt(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
-const submitForm = () => {
-  if(isFormValid.value) {
-    console.log("Form is valid", user.value);
-  } else {
-    console.log("Form is invalid");
+  return JSON.parse(jsonPayload);
+}
+async function userLogin(user) {
+  try {
+    const res = await fetch(`${url}/v2/user`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        ...user,
+      }),
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    const payload = parseJwt(data.token);
+    console.log("Token Payload:", payload);
+    localStorage.setItem("userPayload", JSON.stringify(payload));
+    return payload;
+  } catch (error) {
+    console.log(`error: ${error}`);
   }
 }
+const submitForm = async () => {
+  const payload = await userLogin(user.value);
+  if (payload) {
+    router.push("/task");
+  } else {
+    showError.value = true;
+    setTimeout(() => {
+      showError.value = false;
+    }, 3000); 
+  }
+};
 </script>
 
 <template>
@@ -66,11 +110,11 @@ const submitForm = () => {
               placeholder="Enter your Username"
               class="input input-bordered text-sm w-[20rem] bg-white"
             />
-            <!-- <div class="label">
-              <span class="label-text-alt">
-                Username cannot more than 50
-              </span>
-            </div> -->
+            <div v-show="!validateUsername" class="label">
+              <span class="label-text-alt text-red-500"
+                >Username must be between 1 and 50 characters</span
+              >
+            </div>
           </label>
           <label class="form-control w-full max-w-xs">
             <div class="label">
@@ -84,17 +128,37 @@ const submitForm = () => {
               placeholder="Enter your password"
               class="input input-bordered text-sm w-[20rem] bg-white"
             />
-            <!-- <div class="label">
-              <span class="label-text-alt visible"
-                >Password cannot more than 50
-              </span>
-            </div> -->
+            <div v-show="!validatePassword" class="label">
+              <span class="label-text-alt text-red-500"
+                >Password must be between 1 and 14 characters</span
+              >
+            </div>
           </label>
-          <button class="w-[20rem] btn btn-primary text-white mt-6"
-          :disabled="!isFormValid"
-          @click="submitForm">
+          <button
+            class="w-[20rem] btn btn-primary text-white mt-6"
+            :disabled="!isFormValid"
+            @click="submitForm"
+          >
             Login
           </button>
+          <div v-show="showError" class="my-2">
+            <div role="alert" class="alert alert-error">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>Error! <br/>InValid Username and Password</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
