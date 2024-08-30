812316759937fe5433314c515c9356da81253fd5
The4Child <tanawin.keer@mail.kmutt.ac.th>
2024-08-30 17:17:12 +0700

    config delete unused class in back end

--- Integrated-BackEnd/src/main/java/com/example/integratedbackend/Kradankanban/TasksRepositories.java
+++
@@ -1,6 +0,0 @@
-package com.example.integratedbackend.Kradankanban;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TasksRepositories extends JpaRepository<Tasks, Integer> {
-}
