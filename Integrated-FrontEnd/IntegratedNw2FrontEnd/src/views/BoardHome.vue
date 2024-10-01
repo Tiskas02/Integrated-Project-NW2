9cf02fadd73ed5bc5eff3d566d62645d1ef79a2e
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2024-10-01 16:50:08 +0700

    check console

--- Integrated-FrontEnd/IntegratedNw2FrontEnd/src/views/BoardHome.vue
+++ Integrated-FrontEnd/IntegratedNw2FrontEnd/src/views/BoardHome.vue
@@ -60,7 +60,7 @@ const addBoard = async (newBoard) => {
   }
 }
 const navigateToBoardTasks = (boardId) => {
-  console.log(boardId);
+  console.log(boardId , `${boardId} boardId`);
   router.push({ name: "Task", params: { id: boardId } })
 }
 </script>
