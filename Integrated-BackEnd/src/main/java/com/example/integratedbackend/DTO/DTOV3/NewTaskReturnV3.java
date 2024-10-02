44c29a8c482b68cebb8c74ca8febd1095ea82424
Tanawin <tanawin.keer@mail.kmutt.ac.th>
2024-10-02 23:13:23 +0700

    Update NewTaskReturnV3.java

--- Integrated-BackEnd/src/main/java/com/example/integratedbackend/DTO/DTOV3/NewTaskReturnV3.java
+++ Integrated-BackEnd/src/main/java/com/example/integratedbackend/DTO/DTOV3/NewTaskReturnV3.java
@@ -1,10 +1,9 @@
 package com.example.integratedbackend.DTO.DTOV3;
 
-import com.example.integratedbackend.Kradankanban.kradankanbanV3.Entities.Boards;
+
 import com.example.integratedbackend.Kradankanban.kradankanbanV3.Entities.StatusV3;
 import lombok.Data;
 
-import java.time.ZonedDateTime;
 
 @Data
 public class NewTaskReturnV3 {
