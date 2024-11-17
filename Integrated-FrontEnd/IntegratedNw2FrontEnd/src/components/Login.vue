7f43735b1c508508b888f3161bbd416870b081ae
Saksit tatitrisakul <saksit.tati@mail.kmutt.ac.th>
2024-11-17 15:50:21 +0700

    Update Login.vue

--- Integrated-FrontEnd/IntegratedNw2FrontEnd/src/components/Login.vue
+++ Integrated-FrontEnd/IntegratedNw2FrontEnd/src/components/Login.vue
@@ -68,32 +68,36 @@ const submitForm = async () => {
   <div
     class="w-full max-h-screen flex flex-col tablet:flex-row my-16 tablet:my-0"
   >
-  <div class="w-full tablet:h-screen flex justify-center tablet:items-center">
-  <div class="flex items-center space-x-2 justify-center">
-    <!-- Icon -->
-    <img class="w-[25%]" src="/icon.png" alt="icon" />
+    <div class="w-full tablet:h-screen flex justify-center tablet:items-center">
+      <div class="flex items-center space-x-2 justify-center">
+        <!-- Icon -->
+        <img class="w-[25%]" src="/icon.png" alt="icon" />
 
-    <!-- Text with typing animation -->
-    <div class="relative w-fit overflow-hidden tablet:overflow-auto laptop:overflow-hidden flex items-center">
-      <div class="flex items-center ">
-        <span
-          class="block text-xl font-bold whitespace-nowrap animate-typing"
-          style="white-space: nowrap"
+        <!-- Text with typing animation -->
+        <div
+          class="relative w-fit overflow-hidden tablet:overflow-auto laptop:overflow-hidden flex items-center"
         >
-          IT-Bangmod Kradan Kanban
-        </span>
-        <!-- Blinking cursor with fade-out effect -->
-        <span
-          class="absolute inset-y-0 right-0 border-r-2 border-black animate-blink animate-duration-[800ms] animate-delay-[0ms]"
-        ></span>
+          <div class="flex items-center">
+            <span
+              class="block text-xl font-bold whitespace-nowrap animate-typing"
+              style="white-space: nowrap"
+            >
+              IT-Bangmod Kradan Kanban
+            </span>
+            <!-- Blinking cursor with fade-out effect -->
+            <span
+              class="absolute inset-y-0 right-0 border-r-2 border-black animate-blink animate-duration-[800ms] animate-delay-[0ms]"
+            ></span>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
 
     <div class="w-full p-4 flex justify-center items-center">
       <div class="tablet:border tablet:p-24 tablet:rounded-xl tablet:bg-white">
-        <div class="text-center font-sans font-medium text-3xl text-slate-700 mobile:mt-12 tablet:mt-0">
+        <div
+          class="text-center font-sans font-medium text-3xl text-slate-700 mobile:mt-12 tablet:mt-0"
+        >
           Welcome
         </div>
         <div
@@ -194,101 +198,6 @@ const submitForm = async () => {
       </div>
     </div>
   </div>
-
-  <!-- <div class="w-full h-screen flex justify-center items-center">
-    <div class="w-[65%] h-[80%] py-28">
-      <div class="w-full flex justify-center my-4">
-        <img class="w-[30%]" src="/icon.png" alt="icon" />
-      </div>
-      <div class="w-full flex justify-center">
-        <div
-          class="text-white font-rubik text-4xl font-bold text-center shadow-md"
-        >
-          IT-Bangmod<br />
-          Kradan Kanban
-        </div>
-      </div>
-    </div>
-    <div
-      class="w-[35%] h-[80%] bg-white rounded-2xl shadow-xl flex justify-center items-center mx-32"
-    >
-      <div>
-        <div
-          class="text-slate-700 font-rubik font-semibold text-3xl text-center w-[20rem] mb-4"
-        >
-          Welcome
-        </div>
-        <div>
-          <label class="form-control w-full max-w-xs my-2">
-            <div class="label">
-              <span class="label-text font-semibold text-slate-500"
-                >Username</span
-              >
-            </div>
-            <input
-              type="text"
-              v-model="user.userName"
-              placeholder="Enter your Username"
-              class="itbkk-username input input-bordered text-sm w-[20rem] bg-white"
-              :maxlength="50"
-            />
-            <div v-show="!validateUsername" class="label">
-              <span class="label-text-alt text-red-500"
-                >Username must be between 1 and 50 characters</span
-              >
-            </div>
-          </label>
-          <label class="form-control w-full max-w-xs my-2">
-            <div class="label">
-              <span class="label-text font-semibold text-slate-500"
-                >Password</span
-              >
-            </div>
-            <input
-              type="password"
-              v-model="user.password"
-              placeholder="Enter your password"
-              class="itbkk-password input input-bordered text-sm w-[20rem] bg-white"
-              :maxlength="14"
-            />
-            <div v-show="!validatePassword" class="label">
-              <span class="label-text-alt text-red-500"
-                >Password must be between 1 and 14 characters</span
-              >
-            </div>
-          </label>
-          <button
-            class="itbkk-button-signin w-[20rem] btn bg-gradient-to-r from-[#023E8A] to-[#3581B9] text-white mt-6"
-            :class="{ disabled: !isFormValid }"
-            :disabled="!isFormValid"
-            @click="submitForm"
-          >
-            Sign In
-          </button>
-          <div v-show="showError" class="itbkk-message my-2">
-            <div role="alert" class="alert alert-error">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 shrink-0 stroke-current"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span class="itbkk-message"
-                >Username or Password is incorrect</span
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <style scoped>
